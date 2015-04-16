e4b0acf76fd225d779307fd075f4d669e3be103f
Frank Higgins <frank.higgins@gmail.com>
2015-04-16 09:55:47 -0700

    Add S3 for paperclip uploads

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'https://frank-pinteresting.herokuapp.com/' }
 
+  # set paperclip to load images to S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
   # Paperclip config:
 
 end
